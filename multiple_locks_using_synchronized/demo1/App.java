097c43bd0b09548aa5e7b715f2484eb903c07b4e
Robinson Severo <robinson.severo@dbccompany.com.br>
2019-07-09 18:44:45 -0300

    Multiple Locks; Using Synchronized

---
+++ multiple_locks_using_synchronized/demo1/App.java
@@ -0,0 +1,7 @@
+package multiple_locks_using_synchronized.demo1;
+
+public class App {
+    public static void main(String[] args) {
+        new Worker().main();
+    }
+}
---
+++ the_synchronized_keyword/demo2/App.java
@@ -0,0 +1,4 @@
+package the_synchronized_keyword.demo2;
+
+public class App {
+}
