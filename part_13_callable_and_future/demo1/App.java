8466dadfd30458154fc744a801e393056ac54883
robinsonvs <robinsonvs@gmail.com>
2019-07-29 00:44:28 -0300

    add part_13_callable_and_future

---
+++ part_13_callable_and_future/demo1/App.java
@@ -0,0 +1,46 @@
+package part_13_callable_and_future.demo1;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.*;
+
+public class App {
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        Future<?> future = executor.submit(new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                Random random = new Random();
+                int duration = random.nextInt(4000);
+
+                if (duration > 2000) {
+                    throw new IOException("Sleeping for too long.");
+                }
+
+                System.out.println("Starting ...");
+
+                try {
+                    Thread.sleep(duration);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                System.out.println("Finished.");
+
+                return null;
+            }
+        });
+
+        executor.shutdown();
+
+        try {
+            System.out.println("Result is : " + future.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            IOException ex = (IOException) e.getCause();
+            System.out.println(ex.getMessage());
+        }
+    }
+}
---
+++ part_13_callable_and_future/demo2/App.java
@@ -0,0 +1,4 @@
+package part_13_callable_and_future.demo2;
+
+public class App {
+}
