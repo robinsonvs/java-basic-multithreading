8c7fbc5e6a795113f4f80132f45e992da4ece3aa
robinsonvs <robinsonvs@gmail.com>
2019-07-28 23:36:17 -0300

    add part_12_semaphores

---
+++ part_12_semaphores/demo1/App.java
@@ -0,0 +1,23 @@
+package part_12_semaphores.demo1;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        ExecutorService executor = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < 200; i++) {
+            executor.submit(new Runnable() {
+                public void run() {
+                    Connection.getInstance().connect();
+                }
+            });
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(1, TimeUnit.DAYS);
+    }
+}
---
+++ part_12_semaphores/demo2/App.java
@@ -0,0 +1,4 @@
+package part_12_semaphores.demo2;
+
+public class App {
+}
