5fce13459ddab046544fb6e13311f54506403a87
robinsonvs <robinsonvs@gmail.com>
2019-08-01 10:05:55 -0300

    update codes part 08

--- part_08_wait_notify/demo1/App.java
+++ part_08_wait_notify/demo1/App.java
@@ -1,5 +1,16 @@
 package part_08_wait_notify.demo1;
 
+
+/**
+ *
+ * {@link Object#wait()} and {@link Object#notify()} in Java; low-level
+ * multi-threading methods of the {@link java.lang.Object} class
+ * that allow you to have one or more threads sleeping, only to be woken up by
+ * other threads at the right moment. Extremely useful for avoiding those
+ * processor-consuming "polling loops".
+ * <br><br>
+ *
+ */
 public class App {
     public static void main(String[] args) {
 
@@ -9,9 +20,7 @@ public class App {
             public void run() {
                 try {
                     processor.produce();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                } catch (InterruptedException ignored) {}
             }
         });
 
@@ -19,9 +28,7 @@ public class App {
             public void run() {
                 try {
                     processor.consume();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                } catch (InterruptedException ignored) {}
             }
         });
 
@@ -31,8 +38,6 @@ public class App {
         try {
             t1.join();
             t2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        } catch (InterruptedException ignored) {}
     }
 }
--- part_08_wait_notify/demo1/Processor.java
+++ part_08_wait_notify/demo1/Processor.java
@@ -2,11 +2,35 @@ package part_08_wait_notify.demo1;
 
 import java.util.Scanner;
 
+/**
+ *
+ * Some background knowledge<br>
+ * Source: <em>http://www.programcreek.com/2009/02/notify-and-wait-example/</em>
+ * <br><br>
+ * {@code synchronized} keyword is used for exclusive accessing. To make a
+ * method {@code synchronized}, simply add the {@code synchronized} keyword to its
+ * declaration.<be>
+ * Then no two invocations of synchronized methods on the same object can
+ * interleave with each other.
+ * <br>
+ * Synchronized statements must specify the object that
+ * provides the intrinsic lock. When {@code synchronized(this)} is used, you
+ * have to avoid to synchronizing invocations of other objects' methods.
+ * <br>
+ * {@link Object#wait()} tells
+ * the calling thread to give up the lock and go to sleep (not polling) until
+ * some other thread enters the same lock and calls {@link Object#notify()}.
+ * <br>
+ * {@link Object#notify()} wakes up the first thread that called wait() on
+ * the same object.
+ * <br><br>
+ *
+ */
 public class Processor {
     public void produce() throws InterruptedException {
         synchronized (this) {
             System.out.println("Producer thread running ...");
-            wait();
+            wait();//this.wait() is fine.
             System.out.println("Resumed.");
         }
     }
@@ -21,6 +45,7 @@ public class Processor {
             System.out.println("Return key pressed.");
             notify();
             Thread.sleep(5000);
+            System.out.println("Consumption done.");
         }
     }
 }
